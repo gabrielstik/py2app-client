1ec6b53b120892d64f1a25a0ae12207fa9405095
gabrielstik <gabriel.stik@gmail.com>
2017-06-14 21:13:44 +0200

    :sparkles: Simple setup.py easy-create client
    Signed-off-by: gabrielstik <gabriel.stik@gmail.com>

--- py2app-client.py
+++ py2app-client.py
@@ -3,10 +3,48 @@
 import os
 import sys
 if sys.version_info < (3, 0):
-    import Tkinter as tk
+    from Tkinter import *
 else:
-    import tkinter as tk
+    from tkinter import *
+import tkFileDialog
 
-root = tk.Tk()
+labelPadY = (20,0)
+submitPadY = 20
+backgroundColor = "#0000ff"
+textColor = "#ffffff"
+
+def build():
+    setup = "from setuptools import setup\n\nAPP = ['"+pathPy+"']\nDATA_FILES = []\nOPTIONS = {'argv_emulation': True,\n\t'iconfile': '"+pathImg+"'}\nsetup(\n\tapp=APP,\n\tdata_files=DATA_FILES,\n\toptions={'py2app': OPTIONS},\n\tsetup_requires=['py2app']\n)"
+    setupFile = open("setup.py","w")
+    setupFile.write(setup)
+    setupFile.close()
+
+def setPath():
+    global pathPy
+    pathPy = tkFileDialog.askopenfilename()
+
+def setIcon():
+    global pathImg
+    pathImg = tkFileDialog.askopenfilename()
+
+root = Tk()
+root.configure(bg=backgroundColor)
 root.title("Py2App Client")
-tk.mainloop()
+browseLabel = Label(root, text="Browse .py", bg=backgroundColor, fg=textColor)
+browseLabel.pack(pady=labelPadY)
+browse = Button(root, text="Browse", command=setPath, highlightbackground=backgroundColor)
+browse.pack()
+nameLabel = Label(root, text="Name your app", bg=backgroundColor, fg=textColor)
+nameLabel.pack(pady=labelPadY)
+nameTv = StringVar()
+name = Entry(root, textvariable=nameTv, highlightbackground=backgroundColor)
+name.pack()
+nameTv.set("My App")
+nameV = nameTv.get()
+iconLabel = Label(root, text="Set icon", bg=backgroundColor, fg=textColor)
+iconLabel.pack(pady=labelPadY)
+icon = Button(root, text="Browse", command=setIcon, highlightbackground=backgroundColor)
+icon.pack()
+make = Button(root, text="Build my app", command=build, highlightbackground=backgroundColor)
+make.pack(pady=submitPadY)
+root.mainloop()
