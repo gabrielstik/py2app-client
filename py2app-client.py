f4c257c199f0b8222805ce339b26cc97b8378f8b
gabrielstik <gabriel.stik@gmail.com>
2017-06-14 20:23:13 +0200

    :tada: Initial commit
    Signed-off-by: gabrielstik <gabriel.stik@gmail.com>

---
+++ py2app-client.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+if sys.version_info < (3, 0):
+    import Tkinter as tk
+else:
+    import tkinter as tk
+
+root = tk.Tk()
+root.title("Py2App Client")
+tk.mainloop()
